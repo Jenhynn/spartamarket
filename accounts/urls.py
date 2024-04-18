42310e09b1ac4c6ee3bb18e7a2432c2f4aac849d
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-04-18 17:08:24 +0900

    accounts 작업 시작

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accounts/', views.accounts),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def accounts(request):
+def signup(request):
     pass
